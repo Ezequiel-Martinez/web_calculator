314bc5936c7443eb5201240fd8651cb49a905a82
Eze <zezequielmartinez@gmail.com>
2021-02-03 11:36:39 -0300

    Modo nocturno, algunos bugs fixed pero quedan otros

--- code.js
+++ code.js
@@ -1,10 +1,13 @@
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 
-let screenEl = document.getElementById("screen");
+let screenEl = document.getElementById("input");
+let historyEl = document.getElementById("history");
 
+historyEl.innerText = null;
 let inProcess = false;
 let isNumberBeginning = true;
+let isChangingOpinion = false;
 let isDecimal = false;
 let firstNumber = null;
 let secondNumber = null;
@@ -60,23 +63,88 @@ function operateNestedEquals() {
         result = result / parseFloat(secondNumber);
 }
 
+function clear() {
+    inProcess = false;
+    isNumberBeginning = true;
+    isChangingOpinion = false;
+    firstNumber = null;
+    secondNumber = null;
+    result = null;
+    operation = null;
+    isDecimal = false;
+    screenEl.innerText = 0;
+    historyEl.innerText = null;
+}
+
+function updateHistory(buttonPressed) {
+    if (buttonPressed.id != "equal") {
+        if (historyEl.innerText == "")
+            historyEl.innerText = firstNumber;
+
+        if (!isChangingOpinion) {
+            if (operation == "substract")
+                historyEl.innerText += " -";
+            
+            if (operation == "add") 
+                historyEl.innerText += " +";
+            
+            if (operation == "multiplication")
+                historyEl.innerText += " *";
+            
+            if (operation == "division")
+                historyEl.innerText += " /";
+        }
+        else {
+            historyEl.innerText = historyEl.innerText.substring(0, historyEl.innerText.length - 1);
+
+            if (operation == "substract")
+                historyEl.innerText += " -";
+            
+            if (operation == "add") 
+                historyEl.innerText += " +";
+            
+            if (operation == "multiplication")
+                historyEl.innerText += " *";
+            
+            if (operation == "division")
+                historyEl.innerText += " /";
+        }
+    }
+    else 
+        historyEl.innerText = "( " + historyEl.innerText + " " + secondNumber + " )";
+}
+
 function operatorClicked() {
+
+    // PRESIONADO CLEAR //
+
     if (this.id == "clear") {
-        inProcess = false;
-        isNumberBeginning = true;
-        firstNumber = null;
-        secondNumber = null;
-        result = null;
-        operation = null;
-        isDecimal = false;
-        screenEl.innerText = 0;
+        clear();
         return;
     }
 
+    // PRESIONADO DELETE //
+    
+    if (this.id == "delete") {
+        if (screenEl.innerText.length == 1) {
+            isNumberBeginning = true;
+            screenEl.innerText = "0";
+        }
+        else 
+            screenEl.innerText = screenEl.innerText.substring(0, screenEl.innerText.length - 1);
+        
+        return;
+    }
+
+    // PRIMER ARGUMENTO //
+
     if (!inProcess) {
+
+        // Operar cuando el usuario clickee el igual después de una operación
 
         if (this.id == "equal" && result != null) {
             operateNestedEquals();
+            historyEl.innerText = null;
             screenEl.innerText = result;
             return;
         }
@@ -85,23 +153,39 @@ function operatorClicked() {
 
         inProcess = true;
         operation = this.id;
-
         firstNumber = screenEl.innerText;
         isNumberBeginning = true;
+
+        updateHistory(this);
     }
 
-    if (inProcess && this.id == "equal") {
-        secondNumber = screenEl.innerText;
+    // SEGUNDO ARGUMENTO O CORRIGIENDO OPERACIÓN//
 
-        operate();
+    if (inProcess) {
 
-        screenEl.innerText = result;
+        // CORRIGIENDO OPERACIÓN //
 
-        isDecimal = false;
-        isNumberBeginning = true;
-        inProcess = false;
+        if (this.id == "substract" || this.id == "add" || this.id == "multiplication" || this.id == "division") {
+            operation = this.id;
+            isChangingOpinion = true;
+            updateHistory(this);
+            return;
+        }
+
+        if (this.id == "equal") {
+            secondNumber = screenEl.innerText;
+    
+            operate();
+            updateHistory(this);
+    
+            screenEl.innerText = result;
+    
+            isDecimal = false;
+            inProcess = false;
+            isChangingOpinion = false;
+        }
+
     }
-
 }
 
 numbers.forEach(number => {
